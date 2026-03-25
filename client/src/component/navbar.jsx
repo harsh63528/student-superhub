53366e2cf9d6b88a37c48a61fa6f93f258f2cc01
harsh singh <vduharsh123@gmail.com>
2026-03-25 21:47:18 +0530

    file structure setup

---
+++ client/src/component/navbar.jsx
@@ -0,0 +1,9 @@
+export default function Navbar(){
+return(
+    <nav className="flex place-content-between px-2 border-2 rounded-2xl bg-emerald-200">
+        <div className="logo "><h1>logo</h1></div>
+        <div className="options">options</div>
+        <div className="account">account</div>
+    </nav>
+)
+}
