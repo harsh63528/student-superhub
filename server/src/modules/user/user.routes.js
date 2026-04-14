bbee9a5ef41dd9118d0fc308f2732052b8ed00c1
harsh singh <vduharsh123@gmail.com>
2026-04-14 21:15:20 +0530

    changes in usr.router.js

--- server/src/modules/user/user.routes.js
+++ server/src/modules/user/user.routes.js
@@ -3,7 +3,7 @@ import { registerUser,verifyEmail,loginUser } from "./user.controller.js";
 
 const router = Router();
 
-
+//routers for user verification
 router.post('/register', registerUser);
 router.get('/verify/:token',verifyEmail);
 router.post('/login', loginUser);
