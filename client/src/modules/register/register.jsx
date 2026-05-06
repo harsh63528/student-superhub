ce03f0c2a714595937beee3d1f162fe4023b1b5a
harsh singh <vduharsh123@gmail.com>
2026-05-06 21:14:16 +0530

    updated navbar.css

--- client/src/modules/register/register.jsx
+++ client/src/modules/register/register.jsx
@@ -1,7 +1,13 @@
 
-
 const Register = () => {
-   
+   return(
+    <>
+    <form action="" method="post">
+        <label htmlFor="name">name:</label>
+        <input type="text" name="" id="" />
+    </form>
+    </>
+   )
 }
 
 export default Register;
